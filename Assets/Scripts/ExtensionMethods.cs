cbf2a5262ccc9dbf6315e989b5c9c74d482cf84d
alevchenkoVisartech <a.levchenko@visartech.com>
2023-05-09 20:38:33 +0300

    added cubes gpu instanced example

--- Assets/Scripts/ExtensionMethods.cs
+++ Assets/Scripts/ExtensionMethods.cs
@@ -39,14 +39,21 @@ namespace DefaultNamespace
 
         private const float Amplitude = 2f;
         private const float Frequency = 0.5f;
+        private static readonly Quaternion TargetRot = Quaternion.Euler(45f, 45f, 45f);
 
         public static void UpdatePositionAndRotation(this Transform transform, float time)
         {
             var pos = transform.position;
+            var result = pos.CalculatePositionAndRotation(time);
+            transform.SetPositionAndRotation(result.pos, result.rot);
+        }
+
+        public static (Vector3 pos, Quaternion rot) CalculatePositionAndRotation(this Vector3 pos, float time)
+        {
             var factor = Mathf.Sin(Frequency * pos.x + time) + Mathf.Cos(Frequency * pos.z + time);
             float y = 0 + Amplitude * factor;
-            var rot = Quaternion.Slerp(Quaternion.identity, new Quaternion(0.25f, 0.25f, 0.25f, 0), factor);
-            transform.SetPositionAndRotation(new Vector3(pos.x, y, pos.z), rot);
+            var rot = Quaternion.Slerp(Quaternion.identity, TargetRot, factor);
+            return (new Vector3(pos.x, y, pos.z), rot);
         }
     }
 }
---
+++ Assets/Scripts/GpuInstancedUpdates/GpuInstancedCubeUpdater.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace.GpuInstancedUpdates
+{
+    public class GpuInstancedCubeUpdater : MonoBehaviour
+    {
+        [SerializeField] private int _side;
+        [SerializeField] private int _depth;
+        [SerializeField] private Material _material;
+        [SerializeField] private Mesh _mesh;
+        
+        private Matrix4x4[] _matrices;
+        private Vector3[] _positions;
+        private RenderParams _rp;
+
+        private void Start()
+        {
+            var count = _side * _side * _depth;
+            _positions = new Vector3[count];
+            _matrices = new Matrix4x4[count];
+
+            ExtensionMethods.SpawnLoop(_side, _depth, (i, pos) =>
+            {
+                _positions[i] = pos;
+            });
+            
+            _rp = new RenderParams(_material);
+        }
+
+        private const int Limit = 784;
+        
+        private void Update()
+        {
+            var time = Time.time;
+            for (int i = 0; i < _positions.Length; i++)
+            {
+                var result = _positions[i].CalculatePositionAndRotation(time);
+                _matrices[i].SetTRS(result.pos, result.rot, Vector3.one);
+                _positions[i].y = result.pos.y;
+                if (i >= Limit && i % Limit == 0)
+                {
+                    Graphics.RenderMeshInstanced(_rp, _mesh, 0, _matrices, instanceCount:Limit, startInstance:i - Limit);
+                }
+            }
+
+            if (_positions.Length > Limit)
+            {
+                var mod = _positions.Length % Limit;
+                if (mod > 0)
+                {
+                    Graphics.RenderMeshInstanced(_rp, _mesh, 0, _matrices, mod, startInstance:_positions.Length - mod - 1);
+                }
+            }
+        }
+    }
+}
